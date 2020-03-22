676fda77d4a179f94ddba9bed3fe82fc31049766
Viktor Schulz <viktor.schulz@mhp.com>
2020-03-22 05:18:30 +0100

    Init

---
+++ README.md
@@ -0,0 +1,20 @@
+# &Kunst
+
+## Usage
+
+  1.clone this repository<br/>
+  2.open a command prompt and change directory to your project directory (cd <your project directory>)<br/>
+  3.run the app by typing node app<br/>
+  4.go to http://localhost:3000 from your browser<br/>
+  
+** Make sure you already installed node js on your machine.(type "node -v" in your command prompt).
+
+## Other contributions
+- https://github.com/themaruf/nodeJS-Bootstrap-Express-boilerplate
+- Photo by Kat Stokes on Unsplash
+- Photo by Olga Guryanova on Unsplash
+- Photo by Danny Howe on Unsplash
+- Photo by Aditya Chinchure on Unsplash
+- Photo by Krists Luhaers on Unsplash
+- Photo by Maxime Bhm on Unsplash
+- Photo by Spencer Imbrock on Unsplash
---
+++ app.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+
+app.use('/assets',express.static(__dirname + '/assets'));
+
+//for /index page
+app.get('/', function(request,response){
+	response.sendFile('index.html',{root:path.join(__dirname,'./views')});
+});
+
+//for /home page
+app.get('/home', function(request,response){
+	response.sendFile('home.html',{root:path.join(__dirname,'./views')});
+});
+
+//for /about page
+app.get('/about', function(request,response){
+	response.sendFile('about.html',{root:path.join(__dirname,'./views')});
+});
+
+//for /contact page
+app.get('/contact', function(request,response){
+	response.sendFile('contact.html',{root:path.join(__dirname,'./views')});
+});
+
+app.get('/festival', function(request,response){
+	response.sendFile('festival.html',{root:path.join(__dirname,'./views')});
+});
+
+app.get('/stage', function(request,response){
+	response.sendFile('stage.html',{root:path.join(__dirname,'./views')});
+});
+
+app.get('/event', function(request,response){
+	response.sendFile('event.html',{root:path.join(__dirname,'./views')});
+});
+
+app.get('/artist', function(request,response){
+	response.sendFile('artist.html',{root:path.join(__dirname,'./views')});
+});
+
+app.get('/festivals', function(request,response){
+	response.sendFile('festivals.html',{root:path.join(__dirname,'./views')});
+});
+
+app.get('/events', function(request,response){
+	response.sendFile('events.html',{root:path.join(__dirname,'./views')});
+});
+
+app.listen(3000,function(){
+	console.log('Listening at port 3000...');
+});
+
+module.exports = app;
